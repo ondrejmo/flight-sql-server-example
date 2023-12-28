9559b5078cf3885f29b1e615295b2d35afd2e0f0
Ondřej Molík <ondrejmo@protonmail.ch>
2023-12-28 17:21:03 +0100

    remove auth, TLS, unused stuff (not working yet

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,18 @@
-FROM python:3.11.6
-
-ARG TARGETPLATFORM
-ARG TARGETARCH
-ARG TARGETVARIANT
-RUN printf "I'm building for TARGETPLATFORM=${TARGETPLATFORM}" \
-    && printf ", TARGETARCH=${TARGETARCH}" \
-    && printf ", TARGETVARIANT=${TARGETVARIANT} \n" \
-    && printf "With uname -s : " && uname -s \
-    && printf "and  uname -m : " && uname -m
+FROM debian:bookworm
 
 # Install base utilities
 RUN apt-get update && \
-    apt-get dist-upgrade --yes && \
     apt-get install -y \
     build-essential \
     cmake \
     wget \
     gcc \
     git \
+    unzip \
     ninja-build \
     libboost-all-dev \
     libsqlite3-dev \
-    sqlite3 \
-    vim && \
+    sqlite3 && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
 
@@ -49,82 +39,38 @@ RUN apt update && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
 
-# Copy the scripts directory into the image (we copy directory-by-directory in order to maximize Docker caching)
-COPY --chown=app_user:app_user ./scripts ./scripts
-
-# Build/Install DuckDB
+ # Install DuckDB
 ARG DUCKDB_VERSION="v0.9.2"
-RUN scripts/build_duckdb.sh ${DUCKDB_VERSION} "Y"
-
-WORKDIR /tmp
-
-# Setup the AWS Client (so we can copy S3 files to the container if needed)
-RUN case ${TARGETPLATFORM} in \
-         "linux/amd64")  AWSCLI_FILE=https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip  ;; \
-         "linux/arm64")  AWSCLI_FILE=https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip  ;; \
-    esac && \
-    curl "${AWSCLI_FILE}" -o "awscliv2.zip" && \
-    unzip awscliv2.zip && \
-    ./aws/install && \
-    rm -f awscliv2.zip
-
-# Create an application user
-RUN useradd app_user --create-home
+RUN wget "https://github.com/duckdb/duckdb/releases/download/${DUCKDB_VERSION}/libduckdb-linux-`dpkg --print-architecture`.zip" -O /tmp/duckdb.zip && \
+    cd /tmp && \
+    unzip -j /tmp/duckdb.zip && \
+    cp libduckdb* /usr/local/lib/ && \
+    cp duckdb.h /usr/local/include/ && \
+    cp duckdb.hpp /usr/local/include/ && \
+    git clone --branch $DUCKDB_VERSION --single-branch --depth 1 https://github.com/duckdb/duckdb.git && \
+    cp -R duckdb/src/include/duckdb /usr/local/include/ && \
+    rm /tmp/duckdb.zip
 
 ARG APP_DIR=/opt/flight_sql
 
-RUN mkdir --parents ${APP_DIR} && \
-    chown app_user:app_user ${APP_DIR} && \
-    chown --recursive app_user:app_user /usr/local
-
-# Switch to a less privileged user...
-USER app_user
+RUN useradd 1000 --create-home && \
+    mkdir --parents ${APP_DIR} && \
+    chown 1000:1000 ${APP_DIR} && \
+    chown --recursive 1000:1000 /usr/local
 
 WORKDIR ${APP_DIR}
-
-RUN python3 -m venv ${APP_DIR}/venv && \
-    echo ". ${APP_DIR}/venv/bin/activate" >> ~/.bashrc && \
-    . ~/.bashrc && \
-    pip install --upgrade pip setuptools wheel
-
-# Set the PATH so that the Python Virtual environment is referenced for subsequent RUN steps (hat tip: https://pythonspeed.com/articles/activate-virtualenv-dockerfile/)
-ENV PATH="${VIRTUAL_ENV}/bin:${PATH}"
-
-# Copy the scripts directory into the image (we copy directory-by-directory in order to maximize Docker caching)
-COPY --chown=app_user:app_user ./scripts ./scripts
-
-# Get the SQLite3 database file
-RUN mkdir data && \
-    wget https://github.com/lovasoa/TPCH-sqlite/releases/download/v1.0/TPC-H-small.db -O data/TPC-H-small.db
-
-# Install Python requirements
-COPY --chown=app_user:app_user ./requirements.txt ./
-RUN pip install --requirement ./requirements.txt
-
-# Create DuckDB database file
-RUN python "scripts/create_duckdb_database_file.py" \
-           --file-name="TPC-H-small.duckdb" \
-           --file-path="data" \
-           --overwrite-file=true \
-           --scale-factor=0.01
+USER 1000
 
 # Build the Flight SQL application
-COPY --chown=app_user:app_user ./CMakeLists.txt ./
-COPY --chown=app_user:app_user ./src ./src
-COPY --chown=app_user:app_user ./jwt-cpp ./jwt-cpp
+COPY --chown=1000:1000 ./CMakeLists.txt ./
+COPY --chown=1000:1000 ./src ./src
 WORKDIR ${APP_DIR}
-RUN . ~/.bashrc && \
-    mkdir build && \
+RUN mkdir build && \
     cd build && \
     cmake .. -GNinja -DCMAKE_PREFIX_PATH=${ARROW_HOME}/lib/cmake && \
     ninja && \
     mv flight_sql /usr/local/bin
 
-COPY --chown=app_user:app_user ./tls ./tls
-
 EXPOSE 31337
 
-# Run a test to ensure that the server works...
-RUN scripts/test_flight_sql.sh
-
-ENTRYPOINT scripts/start_flight_sql.sh
+ENTRYPOINT flight_sql --backend=duckdb --database-filename="${DATABASE_FILENAME}" --print-queries
--- src/flight_sql.cpp
+++ src/flight_sql.cpp
@@ -17,7 +17,6 @@
 
 #include "sqlite/sqlite_server.h"
 #include "duckdb/duckdb_server.h"
-#include "FlightAuthHandler.h"
 
 
 namespace flight = arrow::flight;
@@ -26,6 +25,7 @@ namespace po = boost::program_options;
 namespace fs = std::filesystem;
 
 const int port = 31337;
+const std::string host = "0.0.0.0";
 
 #define RUN_INIT_COMMANDS(serverType, init_sql_commands) \
     do { \
@@ -44,40 +44,14 @@ const int port = 31337;
 arrow::Result<std::shared_ptr<arrow::flight::sql::FlightSqlServerBase>> CreateServer(
         const std::string &db_type,
         const fs::path &db_path,
-        const fs::path &tls_cert_path,
-        const fs::path &tls_key_path,
-        const fs::path &mtls_ca_cert_path,
         const std::string &init_sql_commands,
         const bool &print_queries
 ) {
     ARROW_ASSIGN_OR_RAISE(auto location,
-                          arrow::flight::Location::ForGrpcTls(arrow::flight::GetFlightServerHostname(), port))
+                          arrow::flight::Location::ForGrpcTcp(host, port))
     std::cout << "Apache Arrow version: " << ARROW_VERSION_STRING << std::endl;
 
     arrow::flight::FlightServerOptions options(location);
-
-    // Setup TLS - we require it because users are sending their passwords
-    ARROW_CHECK_OK(arrow::flight::FlightServerTlsCertificates(tls_cert_path, tls_key_path, &options.tls_certificates));
-
-    // Setup authentication middleware (using the same TLS certificate keypair)
-    auto header_middleware = std::make_shared<arrow::flight::HeaderAuthServerMiddlewareFactory>(
-            options.tls_certificates);
-    auto bearer_middleware = std::make_shared<arrow::flight::BearerAuthServerMiddlewareFactory>(
-            options.tls_certificates);
-
-    options.auth_handler = std::make_unique<arrow::flight::NoOpAuthHandler>();
-    options.middleware.push_back({"header-auth-server", header_middleware});
-    options.middleware.push_back({"bearer-auth-server", bearer_middleware});
-
-    // Ensure a password is set for the server
-    std::string flight_server_password;
-    ARROW_CHECK_OK (arrow::flight::GetFlightServerPassword(&flight_server_password));
-
-    if (!mtls_ca_cert_path.empty()) {
-        std::cout << "Using mTLS CA certificate: " << mtls_ca_cert_path << std::endl;
-        ARROW_CHECK_OK(arrow::flight::FlightServerMtlsCACertificate(mtls_ca_cert_path, &options.root_certificates));
-        options.verify_client = true;
-    }
 
     std::shared_ptr<arrow::flight::sql::FlightSqlServerBase> server = nullptr;
 
@@ -123,9 +97,6 @@ arrow::Result<std::shared_ptr<arrow::flight::sql::FlightSqlServerBase>> CreateSe
 
 arrow::Status Main(const std::string &backend,
                    const fs::path &database_file_name,
-                   const fs::path &tls_cert_path,
-                   const fs::path &tls_key_path,
-                   const fs::path &mtls_ca_cert_path,
                    const std::string &init_sql_commands,
                    const bool &print_queries
 ) {
@@ -142,8 +113,7 @@ arrow::Status Main(const std::string &backend,
     }
 
     ARROW_ASSIGN_OR_RAISE(auto server,
-                          CreateServer(backend, database_file_name, tls_cert_path, tls_key_path, mtls_ca_cert_path,
-                                       new_init_sql_commands, print_queries))
+                          CreateServer(backend, database_file_name, new_init_sql_commands, print_queries))
 
     ARROW_CHECK_OK(server->Serve());
 
@@ -151,9 +121,6 @@ arrow::Status Main(const std::string &backend,
 }
 
 int main(int argc, char **argv) {
-
-    std::vector<std::string> tls_token_values;
-
     // Declare the supported options.
     po::options_description desc("Allowed options");
     desc.add_options()
@@ -162,13 +129,8 @@ int main(int argc, char **argv) {
              "Specify the database backend. Allowed options: duckdb, sqlite.")
             ("database-filename,D", po::value<std::string>()->default_value(""),
              "Specify the database filename (absolute or relative to the current working directory)")
-            ("tls,T", po::value<std::vector<std::string>>(&tls_token_values)->multitoken()->default_value(
-                     std::vector<std::string>{"tls/cert0.pem", "tls/cert0.key"}, "tls/cert0.pem tls/cert0.key"),
-             "Specify the TLS certificate and key file paths.")
             ("init-sql-commands,I", po::value<std::string>()->default_value(""),
              "Specify the SQL commands to run on server startup.")
-            ("mtls-ca-cert-filename,M", po::value<std::string>()->default_value(""),
-             "Specify an optional mTLS CA certificate path used to verify clients.  The certificate MUST be in PEM format.")
             ("print-queries,Q", po::bool_switch()->default_value(false), "Print queries run by clients to stdout");
 
     po::variables_map vm;
@@ -190,36 +152,6 @@ int main(int argc, char **argv) {
         database_file_name = fs::absolute(database_file_name).string();
     }
 
-    std::vector<std::string> tls_tokens = tls_token_values;
-    if (tls_tokens.size() != 2) {
-        std::cout << "--tls is a required argument, and requires 2 entries - separated by a space!" << std::endl;
-        return 1;
-    }
-    fs::path tls_cert_path = tls_tokens[0];
-    fs::path tls_key_path = tls_tokens[1];
-
-    if (tls_cert_path.empty()) {
-        std::cout << "--tls requires a certificate file path as the first entry!" << std::endl;
-        return 1;
-    } else {
-        tls_cert_path = fs::absolute(tls_cert_path).string();
-        if (!fs::exists(tls_cert_path)) {
-            std::cout << "TLS certificate file does not exist: " << tls_cert_path << std::endl;
-            return 1;
-        }
-    }
-
-    if (tls_key_path.empty()) {
-        std::cout << "--tls requires a key file path as the second entry!" << std::endl;
-        return 1;
-    } else {
-        tls_key_path = fs::absolute(tls_key_path).string();
-        if (!fs::exists(tls_key_path)) {
-            std::cout << "TLS key file does not exist: " << tls_key_path << std::endl;
-            return 1;
-        }
-    }
-
     std::string init_sql_commands = vm["init-sql-commands"].as<std::string>();
     if (init_sql_commands.empty()) {
         auto env_init_sql_commands = std::getenv("INIT_SQL_COMMANDS");
@@ -228,20 +160,9 @@ int main(int argc, char **argv) {
         }
     }
 
-    fs::path mtls_ca_cert_path = vm["mtls-ca-cert-filename"].as<std::string>();
-
-    if (!mtls_ca_cert_path.empty()) {
-        mtls_ca_cert_path = fs::absolute(mtls_ca_cert_path).string();
-        if (!fs::exists(mtls_ca_cert_path)) {
-            std::cout << "mTLS CA certificate file does not exist: " << mtls_ca_cert_path << std::endl;
-            return 1;
-        }
-    }
-
     bool print_queries = vm["print-queries"].as<bool>();
 
-    if (auto status = Main(backend, database_file_name, tls_cert_path, tls_key_path, mtls_ca_cert_path,
-                           init_sql_commands, print_queries); !status.ok()) {
+    if (auto status = Main(backend, database_file_name, init_sql_commands, print_queries); !status.ok()) {
         std::cerr << status << std::endl;
         return 1;
     }
